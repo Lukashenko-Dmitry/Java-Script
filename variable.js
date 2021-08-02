dcbeaccf6185546c52633c2dc6c41f440b16fd88
Dmitry Lukashenko <88265797+Lukashenko-Dmitry@users.noreply.github.com>
2021-08-02 16:40:25 +0300

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,4 @@
+"use strict";
+let message;
+message='Hello';//сохранить строку
+alert (message);
---
+++ variable1.js
@@ -0,0 +1,7 @@
+"use strict";
+let message;
+message='Hello';//сохранить строку
+let user='John';
+let age='25';
+let time='evening';
+alert (message+" "+user+". Are you ready for "+age+" at the "+time);
