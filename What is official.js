9961a47bf4c9cea07d3409801c5eb1fb6ac75821
Dmitry Lukashenko <88265797+Lukashenko-Dmitry@users.noreply.github.com>
2021-08-06 22:21:36 +0300

    Add files via upload

---
+++ What is official.js
@@ -0,0 +1,8 @@
+"use strict";
+let name = prompt("What is an official name of JavaScript?");
+if (name == "ECMAScript") {
+  alert("You are right!");
+}
+else {
+	alert ("You don't know! ECMAScript");
+}
