2385fbbe74e73582c7ce715dfac21670b1026b40
Dmitry Lukashenko <88265797+Lukashenko-Dmitry@users.noreply.github.com>
2021-08-06 21:51:42 +0300

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,12 @@
+"use strict";
+let year = prompt("In what year Ukraine took freedom?");
+if (year > 1991) 
+{
+	alert("It's too late")
+}
+else if(year<1991){
+	alert("It's too early")
+}
+else {
+	alert("You are right")
+}
