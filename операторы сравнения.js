49347ed542428c168d5e557f59f5734b18b9b2c6
Dmitry Lukashenko <88265797+Lukashenko-Dmitry@users.noreply.github.com>
2021-08-06 19:36:01 +0300

    Add files via upload

---
+++ операторы сравнения.js
@@ -0,0 +1,13 @@
+"use strict";
+let result=(2>1);
+alert (result);
+
+alert(0===false);
+
+alert(5 > 4);
+alert("ананас" > "яблоко");
+alert("2" > "12");
+alert(undefined == null);
+alert(undefined === null);
+alert(null == "\n0\n");
+alert(null === +"\n0\n");
